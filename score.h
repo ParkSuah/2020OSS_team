9af2d9fe44364121c62d3807a5745c49f2c82eb5
Suah Park <shuajohn@gmail.com>
2020-05-07 19:15:55 +0900

    score.h edited

--- score.h
+++ score.h
@@ -13,7 +13,7 @@ void readScore(Score s); //하나의 데이터를 읽는 함수
 
 int addScore(Score *s); //새로운 식당을 추가하는 함수
 
-int updateScore(Score *s, int count); //기존 등록된 식당을 수정하는 함수
+int updateScore(Score *s); //기존 등록된 식당을 수정하는 함수
 
-int deleteScore(Score *s, int count); //등록된 식당을 제거하는 함수
+int deleteScore(Score *s); //등록된 식당을 제거하는 함수
 

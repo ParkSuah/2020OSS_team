8103f07857ce6626b316b4237d558d01304b7265
Suah Park <shuajohn@gmail.com>
2020-05-07 19:39:09 +0900

    list.h edited

---
+++ list.h
@@ -0,0 +1,10 @@
+#include "score.h"
+
+int scoreMenu();
+void saveData(Score* s, int count);
+int loadData(Score * s);
+void searchAvg(Score * s, int count);
+void searchName(Score * s, int count);
+void searchRank(Score * s, int count);
+void listScore(Score * s, int count);
+int selectDataNo(Score* s, int count);

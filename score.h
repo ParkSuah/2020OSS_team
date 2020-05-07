ae2939b6b7a0c976e0592b05d6b6561695cd63cd
Suah Park <shuajohn@gmail.com>
2020-05-07 19:05:13 +0900

    error correction

--- score.h
+++ score.h
@@ -11,9 +11,9 @@ typedef struct _score{
 
 void readScore(Score s); //하나의 데이터를 읽는 함수
 
-int addData(Score *s); //새로운 식당을 추가하는 함수
+int addScore(Score *s); //새로운 식당을 추가하는 함수
 
-int updateData(Score *s, int count); //기존 등록된 식당을 수정하는 함수
+int updateScore(Score *s, int count); //기존 등록된 식당을 수정하는 함수
 
-int deleteData(Score *s, int count); //등록된 식당을 제거하는 함수
+int deleteScore(Score *s, int count); //등록된 식당을 제거하는 함수
 

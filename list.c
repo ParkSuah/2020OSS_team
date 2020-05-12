e1fc0c6a09b40c301fccfe32abf31868c7bf373f
HyeokyYun <hyoeky98@gmail.com>
2020-05-12 22:51:09 +0900

    list.c

--- list.c
+++ list.c
@@ -19,20 +19,20 @@ int selectDataNo(Score *s, int count){
 }
 
 int scoreMenu(){
-    int num;
-    printf("\n====메뉴목록====\n");
-    printf("1. 조회\n");
-    printf("2. 추가\n");
-    printf("3. 수정\n");
-    printf("4. 삭제\n");
-    printf("5. 파일저장\n");
-    printf("6. 이름검색\n");
-    printf("7. 평균검색\n");
-    printf("8. 등수정렬\n");
-    printf("0. 종료\n");
-    printf("원하는 메뉴는 ? ");
-    scanf("%d", &num);
-    return num;
+	int num;
+  printf("\n====메뉴목록====\n");
+  printf("1. 조회\n");
+  printf("2. 추가\n");
+  printf("3. 수정\n");
+  printf("4. 삭제\n");
+  printf("5. 파일저장\n");
+  printf("6. 이름검색\n");
+  printf("7. 평균검색\n");
+  printf("8. 등수정렬\n");
+  printf("0. 종료\n");
+  printf("원하는 메뉴는 ? ");
+  scanf("%d", &num);
+  return num;
 }
 
 void saveData(Score* s, int count){

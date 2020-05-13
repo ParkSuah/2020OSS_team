86a2be9be0a00083f375118a033ce573ee3700e1
MasicTurtle <61458692+HyeokyYun@users.noreply.github.com>
2020-05-13 22:40:27 +0900

    Update list.c

--- list.c
+++ list.c
@@ -1,84 +1,136 @@
 #include "list.h"
 
-int main(void){
-	Score slist[100];
-	int choice;
-	int count = 0;
-	int curcount = 0;
-	
-	count = loadData(slist);
-	curcount = count;	
-	while(1){
-		choice = scoreMenu();
-		if(choice == 0) break;
-		else if(choice == 1){
-			if(count > 0){
-				listScore(slist, count);
-			}else{
-				printf("아무런 정보도 입력되지 않았습니다.\n");
-			}
-		}
-		else if(choice == 2){
-			count += addScore(&slist[curcount++]);
-		}
-		else if(choice == 3){
-			if(count > 0){
-				int select = selectDataNo(slist, curcount);
-				if(select == 0){
-					printf("==> 수정이 취소됩니다.\n");
-					continue;
-				}
-				updateScore(&slist[select -1]);	
-			}else{
-				printf("아무런 정보도 입력되지 않았습니다.\n");
-			}
-		}
-		else if(choice == 4){
-			int deletok;
-			if(count > 0){
-				int select = selectDataNo(slist, curcount);
-				if(select == 0){
-					printf("==> 삭제가 취소됩니다.\n");
-					continue;
-				}
-				printf("정말 삭제하시겠습니까? (예:1) : ");
-				scanf("%d", &deletok);
-				if(deletok == 1){
-					deleteScore(&slist[select - 1]);
-					count--;
-				}
-				else printf("==> 제품삭제 취소");
-			}else{
-				printf("아무런 정보도 입력되지 않았습니다.\n");
-			}
-		}else if(choice == 5){
-			if(count > 0){
-				saveData(slist, curcount);
-			}else{
-				printf("아무런 정보도 입력되지 않았습니다 .\n");
-			}
-		}else if(choice == 6){
-			if(count > 0)
-				searchName(slist, curcount);
-			else
-				printf("아무런 정보도 입력되지 않았습니다.\n");
-		}else if(choice == 7){
-			if(count > 0)
-				searchAvg(slist, curcount);
-			else
-				printf("아무런 정보도 입력되지 않았습니다.\n");
-		}else if(choice == 8){
-			if(count > 0)
-				searchRank(slist, curcount);
-			else
-				printf("아무런 정보도 입력되지 않았습니다.\n");
-		}else if(choice == 9){
-			if(count > 0)
-				listByScore(slist, curcount);
-			else
-				printf("아무런 정보도 입력되지 않았습니다.\n");
-			}	
+void listScore(Score *s, int count){
+	printf("\n등수       이름      국어    영어    수학    총점     평균      성적\n");
+	printf("======================================================================||\n");
+	for(int i = 0; i < count; i++){
+		if(s[i].kor == -1 && s[i].eng == -1 && s[i].math == -1) continue;
+		rankScore(s, count);
+		printf("%d등       ", s[i].rank);
+		readScore(s[i]);
 	}
-	printf("==>종료됨!\n");
-	return 0;
+	printf("\n");
+}
+
+int selectDataNo(Score *s, int count){
+	int num;
+	listScore(s, count);
+	printf("수정할 정보(취소: 0): ");
+	scanf("%d", &num);
+	return num;
+}
+
+int scoreMenu(){
+    int num;
+    printf("\n====메뉴목록====\n");
+    printf("1. 조회\n");
+    printf("2. 추가\n");
+    printf("3. 수정\n");
+    printf("4. 삭제\n");
+    printf("5. 파일저장\n");
+    printf("6. 이름검색\n");
+    printf("7. 평균검색\n");
+    printf("8. 등수검색\n");
+    printf("0. 종료\n");
+    printf("원하는 메뉴는 ? ");
+    scanf("%d", &num);
+    return num;
+}
+
+void saveData(Score* s, int count){
+	FILE* fp = fopen("data.txt", "wt");
+	for(int i=0; i<count; i++){
+		if(s[i].kor == -1) continue;
+	fprintf(fp, "%d %d %d %d %.1f %s\n", s[i].kor, s[i].eng, 
+		s[i].math, s[i].sum, s[i].avg, s[i].name);
+	}
+	fclose(fp);
+	printf("저장됨!\n");
+}
+
+int loadData(Score* s){
+	int c = 0;
+	FILE* fp = fopen("data.txt", "rt");
+	if(fp == NULL){
+		printf("파일없음\n");
+		return 0;
+	}
+	for(;;c++){
+		fscanf(fp, "%d %d %d %d %f ", &s[c].kor, &s[c].eng, 
+				&s[c].math,&s[c].sum, &s[c].avg);
+		fgets(s[c].name, 20, fp);
+		s[c].name[strlen(s[c].name)-1] = '\0';
+		if(feof(fp)) break;
+	}
+	rankScore(s, c);
+	printf("로딩 성공 !\n");
+	return c;
+}
+
+void searchName(Score *s, int count){
+	int nameCount = 0;
+	char searchName[20];
+	printf("검색할 이름: ");
+	scanf("%s", searchName);
+	
+	printf("\n===================== [검색한 이름] ======================||\n");
+	printf("이름       국어    영어    수학    총점     평균      성적\n");
+	printf("===========================================================||\n");
+	for(int i = 0; i < count; i++){
+		if(s[i].kor == -1 && s[i].math == -1 && s[i].eng == -1)continue;
+		if(strstr(s[i].name, searchName)){
+			readScore(s[i]);
+			nameCount++;
+		}
+	}
+	if(nameCount == 0){
+		printf("==> \"%s\" 는 없습니다.\n", searchName);
+	}
+}
+
+void searchAvg(Score* s, int count){
+	int avgCount = 0;
+	float searchAvg ;
+	printf("검색할 평균 점수(입력 점수 이상인 평균 출력!): ");
+	scanf("%f", &searchAvg);
+	
+	printf("\n===================== [검색한 평균] ======================||\n");
+	printf("이름       국어    영어    수학    총점     평균      성적\n");
+	printf("===========================================================||\n");
+	for(int i=0;i<count;i++){
+		if(s[i].kor == -1 && s[i].math == -1 && s[i].eng == -1) continue;
+		if(s[i].avg >= searchAvg){
+			readScore(s[i]);
+			avgCount++;
+		}
+	}
+	if(avgCount == 0)
+		printf("==> %.1f 이상의 평균점수를 가진 학생은 없습니다.\n", searchAvg);
+}
+
+void searchRank(Score* s, int count){
+		int searchRank;
+		printf("검색할 등수: ");
+		scanf("%d", &searchRank);
+		int c = 0;
+		/*
+		while(c < count){
+			for(int i=0; i<count; i++){
+				if(i+1 == s[i].rank)
+					readScore(s[i]);
+			}
+			c++;
+		}
+		*/
+		if(searchRank > count){
+			printf("학생 수는 %d명입니다.\n", count);
+		}else{
+			printf("\n===================== [검색한 등수] ======================||\n");
+			printf("이름       국어    영어    수학    총점     평균      성적\n");
+			printf("===========================================================||\n");
+			for(int i=0; i<count; i++){
+				if(s[i].rank == searchRank)
+					readScore(s[i]);
+			}
+	}
 }

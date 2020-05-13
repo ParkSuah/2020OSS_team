ee4f0d7137ebb97c36e5b103a82866b00bd6f954
HyeokyYun <hyoeky98@gmail.com>
2020-05-13 23:00:58 +0900

    score.c

--- score.c
+++ score.c
@@ -52,7 +52,7 @@ char getGrade(float avg){
 	return g;
 }
 
-int addScore(Score *s, int count){
+int addScore(Score *s){
 	printf("학생의 이름: ");
 	scanf("%s", s->name);
 	printf("국어점수: ");
@@ -80,7 +80,7 @@ void readScore(Score s){
 		s.name, s.kor, s.eng, s.math, s.sum, s.avg, s.rank, r);
 }
 
-int updateScore(Score *s, int count){
+int updateScore(Score *s){
 	printf("학생의 이름: ");
 	scanf("%s", s->name);
 	printf("국어점수: ");

5ec127b377c67eba713a828728f99cf06e98bec5
HyeokyYun <hyoeky98@gmail.com>
2020-05-13 22:59:08 +0900

    score.h

--- score.h
+++ score.h
@@ -18,10 +18,6 @@ int updateScore(Score *s); //기존 등록된 식당을 수정하는 함수
 
 int deleteScore(Score *s); //등록된 식당을 제거하는 함수
 
-<<<<<<< HEAD
 void rankScore(Score *s, int count); //등수매기기
-=======
 void rankScore(Score* s, int count); // 학생들의 등수 매기기
->>>>>>> 94b3e797a5fca562bd6f3610c57014aea7ea6555
-
 void listByScore(Score* s, int count); // 등수에 따라 나열
